837ea85135ecf19b1193ece30113d5093189648a
CSteigueber <c.steigueber@gmail.com>
2020-02-03 14:38:03 +0100

    check

--- video-app/src/App.js
+++ video-app/src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
+
+
+function getData(){
+  const url = '/discover/movie?primary_release_date.gte=2019-09-15&primary_release_date.lte=2019-10-22';
+  console.log ("check!");
+  axios.get(url).then(response => console.log(response));
+
+}
 
 function App() {
+  getData();
   return (
     <div className="App">
       <p>Movie App</p>
